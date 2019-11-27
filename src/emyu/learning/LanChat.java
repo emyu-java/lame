5ad7b381bccb75a664dd2ecb9ea88a507e5c8979
Mohamed Usman <emyu10@gmail.com>
2019-11-27 11:57:54 +0500

    initial files

---
+++ src/emyu/learning/LanChat.java
@@ -0,0 +1,84 @@
+package emyu.learning;
+
+import emyu.learning.network.BroadcastServerThread;
+import emyu.learning.network.ClientThread;
+import emyu.learning.ui.ChatPane;
+import emyu.learning.ui.UserPane;
+import emyu.learning.ui.UserView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.net.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+
+public class LanChat extends JFrame {
+    private JSplitPane appPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+    private UserPane userPane = new UserPane(this);
+    private ChatPane chatPane = new ChatPane();
+    private InetAddress broadcastIp;
+    private InetAddress localIp;
+    private String username;
+
+    /**
+     * The constructor.
+     */
+    private LanChat() {
+        appPane.setLeftComponent(userPane);
+        appPane.setRightComponent(chatPane);
+        add(appPane);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setBackground(Color.WHITE);
+        setResizable(false);
+        setVisible(true);
+        initialize();
+        userPane.startReceivingBroadCast();
+    }
+
+    private void initialize() {
+        try {
+            Enumeration<NetworkInterface> nfaces = NetworkInterface.getNetworkInterfaces();
+            while (nfaces.hasMoreElements()) {
+                NetworkInterface nface = nfaces.nextElement();
+                java.util.List<InterfaceAddress> ips = nface.getInterfaceAddresses();
+                for (InterfaceAddress a : ips) {
+                    if (a.getBroadcast() != null) {
+                        broadcastIp = a.getBroadcast();
+                        localIp = a.getAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            JOptionPane.showMessageDialog(this, "Could not initialize network connections. Closing the application now.", "Sorry", JOptionPane.ERROR_MESSAGE);
+            this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+        }
+    }
+
+    public InetAddress getLocalIp() {
+        return this.localIp;
+    }
+
+    public InetAddress getBroadcastIp() {
+        return this.broadcastIp;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    /**
+     * main method to launch the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        LanChat c = new LanChat();
+        (new ClientThread()).start();
+    }
+}
---
+++ src/emyu/learning/ui/UserPane.java
@@ -0,0 +1,76 @@
+package emyu.learning.ui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.DatagramPacket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.HashMap;
+import java.util.List;
+
+import emyu.learning.AppConstants;
+import emyu.learning.LanChat;
+import emyu.learning.network.BroadcastServerThread;
+
+public class UserPane extends JScrollPane {
+    private JPanel container = new JPanel();
+    private JViewport viewport;
+    private LanChat parent;
+    private HashMap<InetAddress, UserView> users = new HashMap<>();
+
+    public UserPane(LanChat parent) {
+        this.parent = parent;
+        viewport = getViewport();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        //container.add(new UserView("emyu10", null));
+        viewport.setMinimumSize(new Dimension(200, parent.getHeight()));
+        viewport.setMaximumSize(new Dimension(200, parent.getHeight()));
+        viewport.add(container);
+        setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        JLabel headerLabel = new JLabel("Online Users");
+        headerLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
+        setColumnHeaderView(headerLabel);
+    }
+
+    public void startReceivingBroadCast() {
+        String username = JOptionPane.showInputDialog(parent, "Enter your preferred username");
+        parent.setUsername(username);
+
+        // construct a UserView with broadcast IP because the server needs to send to the broadcast ip
+        (new BroadcastServerThread(new UserView(parent.getUsername(), parent.getBroadcastIp()))).start();
+
+        SwingWorker<Void, DatagramPacket> broadcastClient = new SwingWorker<Void, DatagramPacket>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                while (true) {
+                    MulticastSocket ds = new MulticastSocket(AppConstants.BROADCAST_PORT);
+                    byte[] buf = new byte[1024];
+                    DatagramPacket dp = new DatagramPacket(buf, 1024);
+                    ds.receive(dp);
+                    if (!dp.getAddress().equals(parent.getLocalIp())) {
+                        publish(dp);
+                    }
+                }
+            }
+
+            @Override
+            protected void process(List<DatagramPacket> chunks) {
+                for (DatagramPacket dp: chunks) {
+                    String name = new String(dp.getData(), 0, dp.getLength());
+                    InetAddress ip  = dp.getAddress();
+                    UserView user = new UserView(name, ip);
+                    if (!users.containsKey(ip)) {
+                        users.put(ip, user);
+                        container.add(user);
+                        container.updateUI();
+                        System.out.println(user);
+                    }
+                }
+            }
+        };
+
+        broadcastClient.execute();
+    }
+}
